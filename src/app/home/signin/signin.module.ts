774c1a356dec1309471018066ab2e9b71729e5f9
Renato de Rezende Pereira <rezende.web@gmail.com>
2020-03-27 00:37:28 -0300

    arquivo removido

--- src/app/home/signin/signin.module.ts
+++ src/app/home/signin/signin.module.ts
@@ -3,18 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { LoginComponent } from './login/loign.component';
 import { PoPageLoginModule, PoModalPasswordRecoveryModule } from '@portinari/portinari-templates';
-import { PoButtonModule, PoFieldModule, PoDividerModule } from '@portinari/portinari-ui';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    PoButtonModule,
     PoPageLoginModule,
-    PoModalPasswordRecoveryModule,
-    PoFieldModule,
-    FormsModule,
-    PoDividerModule
+    PoModalPasswordRecoveryModule
   ],
   declarations: [
       LoginComponent
